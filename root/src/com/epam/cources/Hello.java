22e83e77b90c74bb9a84dc6eabcd87963fdcd35f
Yuliaaaaaa <35068089+Yuliaaaaaa@users.noreply.github.com>
2019-03-23 02:39:10 +0200

    Add files via upload

---
+++ root/src/com/epam/cources/Hello.java
@@ -0,0 +1,4 @@
+package com.epam.cources;
+
+public class Hello {
+}
---
+++ root/src/com/epam/cources2/Hello2.java
@@ -0,0 +1,13 @@
+package com.epam.cources2;
+
+import com.epam.cources.Hello;
+
+public class Hello2 {
+    public void create_Hello(){
+        Hello hello = new Hello();
+    }
+
+    public static void main(String[] args) {
+        Hello hello = new Hello();
+    }
+}
